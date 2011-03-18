11d01cb757c522cbb39d28c0f056da885d417493
James Sandford <jamesmsandford@googlemail.com>
2011-03-18 10:33:47 +0000

    Bus routines should be removed

--- Roster.java
+++ Roster.java
@@ -22,26 +22,57 @@ class bus{
     busID = ID;
     runTime = Time;
   }
+  
+  public int getID(){
+    return busID;
+  }
+
+  public int getTime(){
+    return runTime;
+  }
 }
 
 public class Roster{
+  /**
+   *The main roster class. All interfaces to the roster library go here.
+   **/
 
   List <bus> busList = new ArrayList<bus>();
   
   public addBus(int ID, int Time){
+    /**
+     *Add a bus to the internal list.
+     **/
     bus newBus = new bus(ID, Time);
     busList.add(newBus);
     Collections.sort(busList, bytime());
   }
 
   public int getBus(){
+    /**
+     *Get the next bus to be used and remove it from the internal list.
+     **/
     bus nextBus = busList.get(0);
     busList.remove(0);
+    return nextBus.getID();
+  }
+
+  public boolean delBus(int ID){
+    /**
+     *Remove bus from internal list. Returns true if item found and removed.
+     **/
+    for (int i = 0; i < busList.size(); i++){
+      if (busList[i].getID() == ID){
+	busList.remove(i);
+	return true;
+      }
+    }
+    return false;
   }
 
   private class byTime implements java.util.Comparator {
     public int compare(bus first, bus second) {
-      return (first.runTime - second.runTime);
+      return (first.getTime() - second.getTime());
     }
   } 
 
